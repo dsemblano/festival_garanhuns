82826e399d044f24a6c2b51f3719b186adae2713
Daniel Semblano <dsemblano@gmail.com>
2013-07-12 01:40:23 -0300

    correcoes css estilos gerais 5

--- no-results.php
+++ no-results.php
@@ -9,11 +9,15 @@
 ?>
 
 <?php get_search_form(); ?>
-<article id="post-0" class="post no-results not-found">	
+<article id="post-0" class="post no-results not-found">
 	<header class="entry-header">		
 		<h1 class="entry-title"><?php _e( 'Nenhum Resultado Encontrado', 'garanhuns' ); ?></h1>
-		
-		<p><?php _e( 'Ou tente uma das categorias ou mat&eacute;rias mais recentes abaixo, ou talvez novamente a busca acima?', 'garanhuns' ); ?></p>
+	</header><!-- .entry-header -->
+	
+	<div class="entry-content">	
+
+			<?php _e( 'Por favor, tente novamente usando outros termos.', 'garanhuns' ); ?><br>
+			<p><?php _e( 'Ou tente uma das categorias ou mat&eacute;rias mais recentes abaixo, ou talvez novamente a busca acima?', 'garanhuns' ); ?></p>		
 					
 		<?php if ( garanhuns_categorized_blog() ) : // Lista categorias e termos - Only show the widget if site has multiple categories. ?>
 		
@@ -41,22 +45,6 @@
 		<?php //the_widget( 'WP_Widget_Recent_Posts', 'title= ' ); ?>
 		
 		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Nao Encontrado') ) : // Posts?><?php endif; ?>
-		
-	</header><!-- .entry-header -->
+	</div>
 
-	<div class="entry-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'garanhuns' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-			
-			<p><?php _e( 'Por favor, tente novamente usando outros termos.', 'garanhuns' ); ?></p>			
-
-		<?php else : ?>
-
-			<p><?php _e( 'N&atilde;o conseguimos encontrar o que voc&ecirc; estava procurando. Usar a busca pode ajudar.', 'garanhuns' ); ?></p>
-
-		<?php endif; ?>
-	</div><!-- .entry-content -->
 </article><!-- #post-0 .post .no-results .not-found -->
