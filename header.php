6ce067a153416db96acd8c8ead263294b65058fb
Daniel Semblano <dsemblano@gmail.com>
2013-07-13 07:53:19 -0300

    css 1.8.4.1 facebook

--- header.php
+++ header.php
@@ -25,15 +25,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="fb-root"></div>
-
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=505048126234511";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
