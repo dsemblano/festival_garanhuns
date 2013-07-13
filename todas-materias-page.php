705f07bac8f14daad18f4955e452ba57ffd6a905
Daniel Semblano <dsemblano@gmail.com>
2013-07-13 05:29:19 -0300

    css 1.8.2

--- todas-materias-page.php
+++ todas-materias-page.php
@@ -6,36 +6,35 @@ Template Name: Todas Materias
 
 <?php get_header(); ?>
 
-<h2><?php the_title(); ?></h2>
 <div id="primary" class="content-area meio">
-	<div id="content" class="site-content" role="main">
+	<div id="todas-materias" class="site-content">
+	<h2><?php the_title(); ?></h2>
 			<?php
 				while(have_posts()) : the_post();
 				$myposts = get_posts('numberposts=-1');
 				foreach($myposts as $post) :
 			?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class('category-page'); ?>>
-				<section id="categoria-destaque-foto">
-					<div class="img-busca">
-						<a href="<?the_permalink();?>">
-							<?php if ( has_post_thumbnail() ) {
-								the_post_thumbnail( 'category-pages', array( 'title' => ''.get_the_title().'' ));
-							} ?>			
-						</a>
+			<article id="post-<?php the_ID(); ?>">
+				<a href="<?php the_permalink(); ?>">
+						<figure class="img-busca">
+								<?php if ( has_post_thumbnail() ) {
+									the_post_thumbnail( 'category-pages', array( 'title' => ''.get_the_title().'' ));
+								} ?>			
+						</figure>
+						<time datetime="<?php the_time('d/m/y G:i'); ?>">
+							<?php the_time('d/m/y G:i'); ?>
+							<h3><?php the_title(); ?></h3>
+						</time>
+					<div class="entry-content">
+						<?php
+						if (empty($post -> post_excerpt)) {
+							$rapidas_resumo = get_the_excerpt();
+							//echo ucfirst(strtolower(string_limit_words($rapidas_resumo, 10))) . ("<span class=leiamais>... Leia mais</span>");
+						} else { echo get_the_excerpt();
+						}
+				?>
 					</div>
-				</section>
-				<header>
-					<?php the_time('d/m/y') ?> - <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>					
-				</header>
-				<div class="entry-content">
-					<?php
-					if (empty($post -> post_excerpt)) {
-						$rapidas_resumo = get_the_excerpt();
-						//echo ucfirst(strtolower(string_limit_words($rapidas_resumo, 10))) . ("<span class=leiamais>... Leia mais</span>");
-					} else { echo get_the_excerpt();
-					}
-			?>
-				</div>
+				</a>
 			</article>
 		<?php endforeach; endwhile; ?>
 		
@@ -47,5 +46,4 @@ Template Name: Todas Materias
 	<?php get_template_part('template/sidebar_direita'); ?><!-- #sidebar direita -->
 
 </div>
-
 <?php get_footer(); ?>
